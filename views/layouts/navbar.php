39bc5667651beb6de9f0a33b6bb79c1b694fbb08
Fernando Mauricio <fernando.mauricio@am.senac.br>
2019-05-20 14:48:19 -0400

    Alterção para produção

--- views/layouts/navbar.php
+++ views/layouts/navbar.php
@@ -13,27 +13,50 @@ NavBar::begin([
         'class' => 'navbar-inverse navbar-fixed-top',
     ],
 ]);
-echo Navx::widget([
-    'options' => ['class' => 'navbar-nav navbar-right'],
-    'encodeLabels' => false,
-    'items' => [
-        ['label' => '<span class="glyphicon glyphicon-list-alt"></span> Listagem de Suportes', 'url' => ['/solicitacao/index']],
 
-        ['label' => 'Administração', 'items' => [
-            '<li class="dropdown-header">Administração dos Suportes</li>',
-                ['label' => '<span class="glyphicon glyphicon-list-alt"></span> Listagem de Suportes', 'url' => ['/solicitacao/index-adm']],
-                ['label' => '<span class="glyphicon glyphicon-floppy-save"></span> Suportes Finalizados', 'url' => ['/solicitacao/index-finalizados']],
-            ]
-        ],
 
-        ['label' => '<span class="glyphicon glyphicon-user"></span>  Usuário (' . utf8_encode(ucwords(strtolower($session['sess_nomeusuario']))) . ')',
+if($session['sess_codunidade'] == 1) { //ACESSOS GTI
+
+    echo Navx::widget([
+        'options' => ['class' => 'navbar-nav navbar-right'],
+        'encodeLabels' => false,
         'items' => [
-                '<li class="dropdown-header">Área do Usuário</li>',
-                ['label' => 'Versões Anteriores', 'url' => ['/site/versao']],
-                ['label' => 'Sair', 'url' => 'https://portalsenac.am.senac.br/portal_senac/control_base_vermodulos/control_base_vermodulos.php'],
+            ['label' => '<span class="glyphicon glyphicon-list-alt"></span> Listagem de Suportes', 'url' => ['/solicitacao/index']],
+
+            ['label' => 'Administração', 'items' => [
+                '<li class="dropdown-header">Administração dos Suportes</li>',
+                    ['label' => '<span class="glyphicon glyphicon-list-alt"></span> Listagem de Suportes', 'url' => ['/solicitacao/index-adm']],
+                    ['label' => '<span class="glyphicon glyphicon-floppy-save"></span> Suportes Finalizados', 'url' => ['/solicitacao/index-finalizados']],
+                ]
+            ],
+
+            ['label' => '<span class="glyphicon glyphicon-user"></span>  Usuário (' . utf8_encode(ucwords(strtolower($session['sess_nomeusuario']))) . ')',
+            'items' => [
+                    '<li class="dropdown-header">Área do Usuário</li>',
+                    ['label' => 'Versões Anteriores', 'url' => ['/site/versao']],
+                    ['label' => 'Sair', 'url' => 'https://portalsenac.am.senac.br/portal_senac/control_base_vermodulos/control_base_vermodulos.php'],
+                ],
             ],
         ],
-    ],
-]);
+    ]);
+}else{
+
+    echo Navx::widget([
+        'options' => ['class' => 'navbar-nav navbar-right'],
+        'encodeLabels' => false,
+        'items' => [
+            ['label' => '<span class="glyphicon glyphicon-list-alt"></span> Listagem de Suportes', 'url' => ['/solicitacao/index']],
+
+            ['label' => '<span class="glyphicon glyphicon-user"></span>  Usuário (' . utf8_encode(ucwords(strtolower($session['sess_nomeusuario']))) . ')',
+            'items' => [
+                    '<li class="dropdown-header">Área do Usuário</li>',
+                    ['label' => 'Versões Anteriores', 'url' => ['/site/versao']],
+                    ['label' => 'Sair', 'url' => 'https://portalsenac.am.senac.br/portal_senac/control_base_vermodulos/control_base_vermodulos.php'],
+                ],
+            ],
+        ],
+    ]);
+
+}
 NavBar::end();
 ?>
